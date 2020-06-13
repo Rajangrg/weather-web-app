2e7ce6cf54b4f0514ce9addb8b0e41de945b30a7
Rajan G <g.rajan@cbanewzealand.org.nz>
2020-06-14 11:23:45 +1200

    sub-components Form created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 //sub components
 import Title from './Components/Title';
+import Form from './Components/From';
 
 class App extends React.Component{
   render(){
     return (
       <div>
        <Title></Title>
+       <Form></Form>
       </div>
     );
   }
---
+++ src/Components/From.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Form extends React.Component{
+	render(){
+		return (
+			<div>
+					<p>Form Loading</p>
+			</div>
+		);
+	}
+}
+
+export default Form;
+
