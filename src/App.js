c9bd9095bed00be185f75f1c0bdd642d46d897cb
Rajan G <g.rajan@cbanewzealand.org.nz>
2020-06-14 11:19:10 +1200

    sub-components Ttile created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//sub components
+import Title from './Components/Title';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+       <Title></Title>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>Weather App</h1>
+				<p>Find the current tempertaure based on the location</p>
+			</div>
+		);
+	}
+}
+
+export default Title;
